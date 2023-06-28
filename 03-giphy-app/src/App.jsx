60e862f9a389c57d87d1cb6e8738b7573953bd81
Ludwig <peongutierrezjatniel@gmail.com>
2023-06-27 18:59:21 -0600

    03-Giphy

---
+++ 03-giphy-app/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import ImageCard from './components/ImageCard'
+import './App.css'
+import SearchBar from './components/SearchBar'
+
+function App () {
+  const [gifs, setGifs] = useState([]) // Aqui guardo la lista de gifs
+  const API_KEY = import.meta.env.VITE_GIPHY_API_KEY
+
+  // Las llamadas a la API se hacen en useEffect
+  // UseEffect usa un arreglo de dependencias vacio para que solo se ejecute una vez cuando carguen mis componentes
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=30&offset=0&rating=g&bundle=messaging_non_clips`)
+      .then((response) => {
+        return response.json()
+      }).then((results) => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=30&offset=0&rating=g&lang=es&bundle=messaging_non_clips`)
+      .then((response) => {
+        return response.json()
+      }).then((results) => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }
+
+  return (
+
+    <div className='App'>
+      <SearchBar handleSearch={sendSearch} />
+
+      <div className='grid-cards'>
+        {
+          gifs.map((gif) => (
+            <ImageCard
+              key={gif.id}
+              title={gif.title}
+              url={gif.images.fixed_height.url}
+            />
+          ))
+        }
+      </div>
+    </div>
+
+  )
+}
+
+export default App
