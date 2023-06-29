628f98ecd7d24b0836667c3272cc7482dd80e653
Ludwig <peongutierrezjatniel@gmail.com>
2023-06-28 21:56:12 -0600

    04-piquito-de-oro

---
+++ 04-piquito-de-oro/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
+import RoutesIndex from './routes/RoutesIndex'
+
+function App () {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <RoutesIndex />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
